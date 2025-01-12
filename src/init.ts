1e59f168ff87984cf801350e5a02b1b4d6494efb
ytkimirti <yusuftaha9@gmail.com>
2025-01-12 19:54:30 +0300

    fix: install node@15 types

--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,10 @@
-import path from "node:path";
-import os from "node:os";
-import fs from "node:fs";
+/* eslint-disable unicorn/prefer-node-protocol */
+import path from "path";
+import os from "os";
+import fs from "fs";
 import chalk from "chalk";
-import { fileURLToPath } from "node:url";
+
+import { fileURLToPath } from "url";
 import { log } from "./log";
 import { config } from "./config";
 
